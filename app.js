9f3ed05ff710dc9689fe8d3ba6a71fdc8b199be8
khailu <kalebgebre@gmail.com>
2017-04-09 16:54:10 -0500

    //config changes

--- app.js
+++ app.js
@@ -13,12 +13,15 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+//app.set("env","production");
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set("strict routing",true);
 app.set("x-powered-by",false);
 app.set("case sensitive routing",true);
+
 app.set("trust proxy",true);
+app.set("view cache",true);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
